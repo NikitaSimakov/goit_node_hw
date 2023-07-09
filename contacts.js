72bb2ec610f46635243d4d335a68616466022a84
NikitaSimakov <106473909+NikitaSimakov@users.noreply.github.com>
2023-07-09 19:18:46 +0300

    add try..catch to async function

--- contacts.js
+++ contacts.js
@@ -5,23 +5,35 @@ import path from "path";
 export const contactsPath = path.resolve("db", "contacts.json");
 
 export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getContactById = async (contactId) => {
-  const data = await listContacts();
-  const contactById = data.find((contact) => contact.id === contactId);
-  return contactById || null;
+  try {
+    const data = await listContacts();
+    const contactById = data.find((contact) => contact.id === contactId);
+    return contactById || null;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const removeContact = async (contactId) => {
-  const data = await listContacts();
-  const contactIndex = data.findIndex((contact) => contact.id === contactId);
-  const [removeData] = data.splice(contactIndex, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2), "utf-8");
-  if (contactIndex === -1) return null;
-  return removeData;
+  try {
+    const data = await listContacts();
+    const contactIndex = data.findIndex((contact) => contact.id === contactId);
+    const [removeData] = data.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2), "utf-8");
+    if (contactIndex === -1) return null;
+    return removeData;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const addContact = async ({ name, email, phone }) => {
@@ -31,10 +43,14 @@ export const addContact = async ({ name, email, phone }) => {
     email,
     phone,
   };
-  const data = await listContacts();
-  data.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2), "utf-8");
-  return newContact;
+  try {
+    const data = await listContacts();
+    data.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2), "utf-8");
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default {
